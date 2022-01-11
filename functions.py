762f0be8aff067f6a4fc98f668157e084112efff
Maria <80037236+VinMariani@users.noreply.github.com>
2022-01-11 15:20:28 +0300

    корректировки + вывод всех фильмов

--- functions.py
+++ functions.py
@@ -5,10 +5,10 @@ import json
 def movie_title(title):
     '''поиск по названию'''
     con = sqlite3.connect('netflix.db')
-    sqlite_query = "SELECT 'title', 'country', 'release_year', 'listed_in', 'description' FROM netflix " \
-                   f"WHERE 'title' = '{title}'" \
-                   "ORDER BY 'release_year'" \
-                   "LIMIT 1"
+    sqlite_query = "SELECT title, country, release_year, listed_in, description FROM netflix " \
+                   f"WHERE title = {title}" \
+                   f"ORDER BY release_year" \
+                   f"LIMIT 1"
     cur = con.cursor()
     cur.execute(sqlite_query)
     movie = cur.fetchone()
@@ -28,9 +28,9 @@ def movie_title(title):
 def movie_year(year1, year2):
     '''поиск по году выпуска'''
     con = sqlite3.connect('netflix.db')
-    sqlite_query = "SELECT 'title', 'country', 'release_year', 'listed_in', 'description' FROM netflix" \
-                   f"WHERE 'release_year' BETWEEN '{year1}' AND '{year2}'" \
-                   "LIMIT 100 "
+    sqlite_query = f"SELECT title, country, release_year, listed_in, description FROM netflix " \
+                   f"WHERE release_year BETWEEN {year1} AND {year2}" \
+                   f"LIMIT 100 "
     cur = con.cursor()
     cur.execute(sqlite_query)
     data = []
@@ -51,8 +51,8 @@ def movie_year(year1, year2):
 def movie_rating(rating):
     '''поиск по рейтингу для детей'''
     con = sqlite3.connect('netflix.db')
-    sqlite_query = "SELECT 'title', 'rating', 'description' FROM netflix " \
-                   f"WHERE 'rating' = '{rating}'"
+    sqlite_query = f"SELECT title, rating, description FROM netflix " \
+                   f"WHERE rating = {rating}"
     cur = con.cursor()
     cur.execute(sqlite_query)
     data = cur.fetchone()
@@ -69,9 +69,9 @@ def movie_rating(rating):
 def movie_genre(genre):
     '''поиск по жанрам'''
     con = sqlite3.connect('netflix.db')
-    sqlite_query = "SELECT 'title', 'rating', 'description' FROM netflix " \
-                   f"WHERE 'listed_in' LIKE '%{genre}%'" \
-                   "LIMIT 10"
+    sqlite_query = f"SELECT title, rating, description FROM netflix " \
+                   f"WHERE listed_in LIKE %{genre}%" \
+                   f"LIMIT 10"
     cur = con.cursor()
     cur.execute(sqlite_query)
     data = []
@@ -90,8 +90,8 @@ def movie_genre(genre):
 def movie_actors(first_actor, second_actor):
     '''поиск по актерам'''
     con = sqlite3.connect('netflix.db')
-    sqlite_query = "SELECT 'cast' FROM netflix " \
-                   f"WHERE 'cast' LIKE '%{first_actor}%' AND 'cast' LIKE '%{first_actor}%'"
+    sqlite_query = f"SELECT 'cast' FROM netflix " \
+                   f"WHERE 'cast' LIKE %{first_actor}% AND 'cast' LIKE %{first_actor}%"
     cur = con.cursor()
     cur.execute(sqlite_query)
     data = cur.fetchone()
@@ -106,10 +106,10 @@ def movie_actors(first_actor, second_actor):
 def movie_category_and_year(movie_type, release_year, genre):
     '''поиск по жанрам'''
     con = sqlite3.connect('netflix.db')
-    sqlite_query = "SELECT 'title', 'release_year', 'rating', 'description', 'listed_in' FROM netflix " \
-                   f"WHERE 'type' = '{movie_type}'" \
-                   f"AND 'listed_in' LIKE '%{genre}'%" \
-                   f"AND 'release_year' = '{release_year}'"
+    sqlite_query = f"SELECT title, release_year, rating, description, listed_in FROM netflix " \
+                   f"WHERE type = {movie_type}" \
+                   f"AND listed_in LIKE %{genre}%" \
+                   f"AND release_year = {release_year}"
     cur = con.cursor()
     cur.execute(sqlite_query)
     data = []
@@ -124,4 +124,27 @@ def movie_category_and_year(movie_type, release_year, genre):
         data.append(films)
         con.close()
 
+    return data
+
+
+def movies_all():
+    '''все фильмы'''
+    con = sqlite3.connect('netflix.db')
+    sqlite_query = "SELECT * FROM netflix " \
+                   "ORDER BY rating ASC " \
+                   "LIMIT 100 OFFSET 100"
+    cur = con.cursor()
+    cur.execute(sqlite_query)
+    movie = cur.fetchone()
+
+    data = {
+        "title": movie[0],
+        "rating": movie[1],
+        "country": movie[2],
+        "release_year": movie[3],
+        "genre": movie[4],
+        "description": movie[5]
+    }
+
+    con.close()
     return data
